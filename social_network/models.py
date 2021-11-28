0b1a9bf36a70a8b61511cbad3d93ab94354e0b3a
Dmytro Kurylko <DimaKurylko@gmail.com>
2021-11-28 19:16:15 +0200

    first commit

---
+++ social_network/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# class Post(models.Model):
+#     created = models.DateTimeField(auto_now_add=True)
+#     title = models.CharField(max_length=255, blank=False)
+#     post = models.CharField(max_length=)
