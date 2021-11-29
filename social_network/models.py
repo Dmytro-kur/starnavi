be75219a28e7dc5810ee10434fa84c85a5b36aed
Dmytro Kurylko <dimakurylko@gmail.com>
2021-11-29 19:36:27 +0200

    Long term

--- social_network/models.py
+++ social_network/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField  
 
 class Post(models.Model):
+
+    user = models.ForeignKey('User', on_delete='CASCADE', related_name='posts')
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255, blank=False)
     post = RichTextUploadingField(config_name='ckeditor_post')
 
     class Meta:
         ordering = ['created']
+
