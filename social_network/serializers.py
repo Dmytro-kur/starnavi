9e82a03f66f1f6d53619ab9ab1df7524311c1de8
Dmytro Kurylko <dimakurylko@gmail.com>
2021-12-02 19:18:25 +0200

    Interesting

--- social_network/serializers.py
+++ social_network/serializers.py
@@ -1,3 +1,10 @@
 from rest_framework import serializers
-from .models import Post
+from .models import User, Post, Like, Activity
 
+
+class PostSerializer(serializers.ModelSerializer):
+    """Create and update new post"""
+
+    class Meta:
+        model = Post
+        fields = ['user', 'created', 'title', 'content']
