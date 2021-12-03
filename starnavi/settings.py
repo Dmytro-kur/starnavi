70fee233055d8786486f26caed0ca87cb7e38501
Dmytro Kurylko <dimakurylko@gmail.com>
2021-12-03 16:38:07 +0200

    delete time settings

--- starnavi/settings.py
+++ starnavi/settings.py
@@ -132,5 +132,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-DATETIME_FORMAT="%Y-%m-%d %H:%M:%S"
